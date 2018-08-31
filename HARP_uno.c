cd60ba06a083fe5f83350623e458936e55856c0c
Jenil Thakker <jenilkaushik.thakker@sjsu.edu>
2018-08-30 21:04:48 -0700

    Update HARP_uno.c

--- HARP_uno.c
+++ HARP_uno.c
@@ -24,6 +24,11 @@ Adafruit_BMP085 bmp;
 MS5803 sensor(ADDRESS_HIGH); // ADDRESS_HIGH = 0x76 (for ADDRESS_LOW = 0x77)
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
+//ADXL sensor init
+
+int scale = 200; // 3 (±3g) for ADXL337, 200 (±200g) for ADXL377
+boolean micro_is_5V = true;
+
 // ______________For MS5803-14BA: create variables to store results_________________________
 float temperature_c, temperature_f;
 double pressure_abs, pressure_relative, altitude_delta, pressure_baseline;
@@ -169,4 +174,45 @@ void loop() {
   Serial.print(event.orientation.z, 4);
   Serial.println("");
   delay(1000);
+  
+  //------------------------ADXL377-------------------------------------
+  // Get raw accelerometer data for each axis
+  int rawX = analogRead(A0);
+  int rawY = analogRead(A1);
+  int rawZ = analogRead(A2);
+  
+  // Scale accelerometer ADC readings into common units
+  // Scale map depends on if using a 5V or 3.3V microcontroller
+  float scaledX, scaledY, scaledZ; // Scaled values for each axis
+  if (micro_is_5V) // Microcontroller runs off 5V
+  {
+    scaledX = mapf(rawX, 0, 675, -scale, scale); // 3.3/5 * 1023 =~ 675
+    scaledY = mapf(rawY, 0, 675, -scale, scale);
+    scaledZ = mapf(rawZ, 0, 675, -scale, scale);
+  }
+  else // Microcontroller runs off 3.3V
+  {
+    scaledX = mapf(rawX, 0, 1023, -scale, scale);
+    scaledY = mapf(rawY, 0, 1023, -scale, scale);
+    scaledZ = mapf(rawZ, 0, 1023, -scale, scale);
+  }
+  
+  // Print out raw X,Y,Z accelerometer readings
+  Serial.print("X: "); Serial.println(rawX);
+  Serial.print("Y: "); Serial.println(rawY);
+  Serial.print("Z: "); Serial.println(rawZ);
+  Serial.println();
+  
+  // Print out scaled X,Y,Z accelerometer readings
+  Serial.print("X: "); Serial.print(scaledX); Serial.println(" g");
+  Serial.print("Y: "); Serial.print(scaledY); Serial.println(" g");
+  Serial.print("Z: "); Serial.print(scaledZ); Serial.println(" g");
+  Serial.println();
+  
+  delay(2000); // Minimum delay of 2 milliseconds between sensor reads (500 Hz)
+}
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
